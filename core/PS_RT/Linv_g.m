9f8b62fbaf31b86c8b1ea9de4e7f67b902868d3c
Lloyd Fung <lloyd.fung@imperial.ac.uk>
2021-03-01 18:14:45 +0000

    2D Script revised. Init commit.

--- core/PS_RT/Linv_g.m
+++ core/PS_RT/Linv_g.m
@@ -4,10 +4,10 @@ function [g,Linv]=Linv_g(S_profile,Mvor,Mgyro,Mlap,Mint,Msin,n,m)
     g=NaN(nm,N_mesh);
     Linv=NaN(nm,nm,N_mesh);
     for j=1:N_mesh
-        Le=Msin*gather(S_profile(j)*Mvor+Mgyro-Mlap);
+        Le=full(Msin*gather(S_profile(j)*Mvor+Mgyro-Mlap));
         Le(n*m/2+m/2+1,:)=Mint;
         
-        Linv(:,:,j)=inv(full(Le));
+        Linv(:,:,j)=inv(Le);
         
         g(:,j)=Linv(:,:,j)*[zeros(n*m/2+m/2,1);1/2/pi;zeros(n*m/2-m/2-1,1)];
     end
